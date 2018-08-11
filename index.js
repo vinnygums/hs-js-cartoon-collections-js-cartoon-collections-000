81f6a131a9b8874cbec0faf682527c6c9f013383
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-11 14:56:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  var numberedDwarves = ''
+  
+  for (var i = 0; i < dwarves.length; i++) {
+    numberedDwarves += `${i+1}. ${dwarves[i]} `
+  }
+
+  return `${numberedDwarves}`
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i].toUpperCase()
+    planeteerCalls[i] += "!"
+  }
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 function findTheCheese (foods) {
-}
+  var typesOfCheese = ["cheddar", "gouda", "camembert"]
+  
+  for (var i = 0; i < foods.length; i++) {
+    for(var b = 0; b < typesOfCheese.length; b++) {
+      if (foods[i] === typesOfCheese[b]) {
+          return foods[i]
+      }
+      }
+      } return "no cheese!"
+      } 
+      
